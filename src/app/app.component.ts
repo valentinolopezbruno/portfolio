7f518d49370be1783a9556eef20c339c8813b215
valentinolopezbruno <valentinolopezbruno@gmail.com>
2024-05-21 18:46:35 -0300

    cambios valen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'portafoli-angular';
 
   windowWidth: number;
@@ -14,9 +14,12 @@ export class AppComponent {
     this.windowWidth = window.innerWidth;
   }
 
+  // Esta variable es para iterar entre la navbar y el usuario la informacion a mostrar
   InfoSeleccionada: string = 'Información'
+  // Esta variable es la encargada del navbar dentro de portfolio
+  // Los proyectos son separados en el html
+  InfoSeleccionadaProyectos:string = "Todos"
 
- 
   mostrarRedes: boolean = false; 
 
   cambiarInfoSeleccinada(info:string):void{
@@ -25,17 +28,25 @@ export class AppComponent {
 
   mostrarRedesSociales() {
     this.mostrarRedes = !this.mostrarRedes;
-    console.log("asdasdas")
   }
 
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
     this.windowWidth = event.target.innerWidth;
 
-    // Mostrar las redes si el ancho de la pantalla es mayor o igual a 768
-    if (this.windowWidth >= 768) {
+    // Mostrar las redes si el ancho de la pantalla es mayor o igual a 500
+    if (this.windowWidth >= 1250) {
       this.mostrarRedes = true;
     }
+  }
+
+  ngOnInit(): void {
+    // Verificar el ancho de la pantalla al inicio
+    this.windowWidth = window.innerWidth;
+    if (this.windowWidth >= 1250) {
+      this.mostrarRedes = true;
+    }
+   
   }
 
 
